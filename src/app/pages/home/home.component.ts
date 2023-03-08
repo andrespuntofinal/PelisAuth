f5803adaf93f4e0e8e0b053f4642f6d13222b8b6
Andres <andrespuntofinal@gmail.com>
2023-03-08 10:52:47 -0500

    Login v1

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor( private auth: AuthService,
+               private router: Router ) { }
+
+  ngOnInit() {
+  }
+
+  salirApp(){
+
+    
+    this.auth.loguot();
+    this.router.navigateByUrl('/login');
+
+
+  }
+
+}
